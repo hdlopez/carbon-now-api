713e1271ebb049850b39246112522f9923e8e839
Horacio López <hdlopez@gmail.com>
2020-02-15 20:00:21 -0300

    Added Carbon API feature to serverless function

--- api/index.js
+++ api/index.js
@@ -1,4 +1,20 @@
 export default async (req, res) => {
     const { body } = req
-    res.end(`Hello ${body.name}, you just parsed the request body!`)
+
+    // TODO: validate pareters
+    const lang = body.lang || 'auto'; // optional, default = auto
+    const code = body.code;
+
+    // Fetch image
+    const url = CARBON_URL + '?code=' + code + '&l=' + lang;
+    console.log("URL", url);
+    
+    headlessVisit({
+        url,
+        IMAGES_URL
+    });
+
+    res.status(201).send({
+        image_url: IMAGES_URL + 'carbon.png'
+    });
 }
