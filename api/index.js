562867f3df2083895bcb4d2290ef8bf6f33baa4c
Horacio López <hdlopez@gmail.com>
2020-02-16 12:50:31 -0300

    Function only accepts query params

--- api/index.js
+++ api/index.js
@@ -2,11 +2,9 @@ const headlessVisit = require('../helpers/headless-visit');
 const {CARBON_URL, IMAGES_URL} = require('../helpers/globals');
 
 export default async (req, res) => {
-    const { body, query } = req
-
     // TODO: validate pareters
-    const lang = query.lang || body.lang || 'auto'; // optional, default = auto
-    const code = query.lang || body.code;
+    const lang = req.query.lang || 'auto'; // optional, default = auto
+    const code = req.query.code;
 
     // Fetch image
     const url = CARBON_URL + '?code=' + code + '&l=' + lang;
